8b72a0fcb27fbde2544358f7ce0e2df26af1f87a
Filipe Haniel <filipe.hanielloper@gmail.com>
2022-12-16 01:31:04 -0300

    inicio tela splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:app_dw_movies/app/modules/splash/splash_module.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseRemoteConfig.instance.fetchAndActivate();
+  
   runApp(const MyApp());
 }
 
