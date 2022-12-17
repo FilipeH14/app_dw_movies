ae1aab9ecacf7873322b50a0e0b1402f7f0835f6
Filipe Haniel <filipe.hanielloper@gmail.com>
2022-12-17 00:25:02 -0300

    inicio configuração da home, movies e favorites

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_dw_movies/application/ui/filmes_app_icons.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,8 +7,23 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home page')),
       body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie),
+            label: 'Filmes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FilmesAppIcons.heart_empty),
+            label: 'Favoritos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.logout_outlined),
+            label: 'Sair',
+          ),
+        ],
+      ),
     );
   }
 }
