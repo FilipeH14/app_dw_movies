22bfaa8977db8b99fe18f7aa6b2faabc0d08eaaf
Filipe Haniel <filipe.hanielloper@gmail.com>
2022-12-20 14:14:51 -0300

    inicio lista de filmes

---
+++ lib/app/modules/movies/widgets/movies_group.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MoviesGroup extends StatelessWidget {
+  final String title;
+
+  const MoviesGroup({
+    required this.title,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 20),
+          Text(
+            title,
+            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
